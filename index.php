f9baa5815f3424b70171cfecc27a3427542b3892
guillaume-goubel <guillaume.goubel.pro@gmail.com>
2018-11-05 12:07:00 +0100

    project base

---
+++ index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/mdb.min.css" rel="stylesheet">  -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+
+    <!-- PAGE CONTENT -->
+
+    <div id="content">
+
+        <div>
+            <!-- END PAGE CONTENT -->
+
+            <!-- FOOTER -->
+            <footer>
+
+                <div class="container">
+
+                    <div class="footer-nav">
+
+                        <div class="row">
+
+                            <div class="col-4">
+
+                                <h3>About</h3>
+
+                                <nav>
+
+                                    <ul>
+
+                                        <li>About us</li>
+                                        <li>Contact</li>
+                                        <li>Legal</li>
+
+                                    </ul>
+
+                                </nav>
+                            </div>
+
+                            <div class="col-4">
+
+                                <h3>Navigation</h3>
+
+                                <nav>
+
+                                    <ul>
+
+                                        <li>Home</li>
+                                        <li>Our Secrveices</li>
+                                        <li>Blog</li>
+
+                                    </ul>
+
+                                </nav>
+                            </div>
+
+                            <div class="col-4">
+
+                                <h3>News</h3>
+
+                                <nav>
+
+                                    <ul>
+
+                                        <li>News 1</li>
+                                        <li>News 2</li>
+                                        <li>News 3</li>
+
+                                    </ul>
+
+                                </nav>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="footer-legal">
+                        &copy; copyright 2018
+
+                    </div>
+
+                </div>
+
+
+            </footer>
+
+            <div> </div>
+
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+                crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+                crossorigin="anonymous"></script>
+            <!-- <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/mdb.min.js"></script> -->
+            <script src="assets/js/app.js"></script>
+            </body>
+
+</html>
